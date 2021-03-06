05cd428d1daf3da0e8f524ae8f4a66e706e284a6
Nilesh-agrahyah <nilesh@agrahyah.com>
2021-03-06 12:36:27 +0530

    Fixing Page Reload Issues #159

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  feature = 'recipe';
-
-  onNavigate(feature){
-    this.feature = feature
-  }
 }
