2e42707d47b0466d6eb36fef57f677649590db19
Nilesh-agrahyah <nilesh@agrahyah.com>
2021-03-06 09:41:38 +0530

    adding ingredients to recipe details #122

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import {EventEmitter, Injectable} from '@angular/core';
 import {Recipe} from '../recipes/recipe.model'
+import { Ingredient } from '../shared/ingredient.model';
 // @Injectable()
 
 export class RecipeService {
@@ -8,8 +9,20 @@ export class RecipeService {
     selectedRecipe = new EventEmitter<Recipe>()
 
     private recipes: Recipe[] = [
-        new Recipe('Jackfruit Burrito', "Recipe of how to create a Jackfruit Burrito that'll make your taste-buds happy!", 'https://images.squarespace-cdn.com/content/v1/556ddd04e4b0975c0bb32168/1504268869937-8JS0CP1IO349FXJQPKCI/ke17ZwdGBToddI8pDm48kJ8sk66WRapo3RX4khTsXKp7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTmaUzSiviepfuOufnJa7SEDbj5B0_nllQ7Wz_LLSnnCyLIYUXv0PgTl9KTvOesAWW4/Jackfruit-Burritos-02-2.jpg'),
-        new Recipe('Burrito', "Recipe of how to create a Burrito that'll make your taste-buds happy!", 'https://image.freepik.com/free-photo/burritos-wraps-with-beef-vegetables-wood-beef-burrito-mexican-food-healthy-food-background-mexican-cuisine_2829-4110.jpg')
+        new Recipe('Tasty Schnitzel',
+         "A super-tasty Schnitzel - just awesome!",
+          'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+          [
+            new Ingredient('Meat', 1),
+            new Ingredient('French Fries', 20),
+          ]),
+        new Recipe('Big Fat Burger',
+         "What else you need to say?",
+          'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+          [
+            new Ingredient('Meat', 1),
+            new Ingredient('Buns', 2),
+          ])
       ];
     
     getRecipes(){
