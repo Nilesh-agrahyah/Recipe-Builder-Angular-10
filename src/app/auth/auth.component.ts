198151b461207073dbfdd13a58a1c93e603d69bb
Nilesh-agrahyah <nilesh@agrahyah.com>
2021-03-17 12:58:08 +0530

    290. Handling Form Input

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { NgForm } from '@angular/forms';
 
 @Component({
     selector: 'app-auth',
     templateUrl: './auth.component.html'
 })
 
-export class AuthComponent {
+export class AuthComponent implements OnInit{
     isLoginMode = true;
+
+    ngOnInit(){
+        
+    }
 
     onSwitchMode(){
         this.isLoginMode = !this.isLoginMode;
+    }
+    onSubmit(form: NgForm){
+        console.log(form.value);
+        form.reset()
     }
 }
