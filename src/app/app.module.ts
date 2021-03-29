79c9c88729d955837ac1a9c0dfe4f78cbd3a07bb
Nilesh-agrahyah <nilesh@agrahyah.com>
2021-03-29 13:27:31 +0530

    323. Splitting Modules Correctly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,35 +5,26 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirectiveDirective } from './directives/dropdown-directive.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component'
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component'
 import { AuthComponent } from './auth/auth.component'
 import { RecipeService } from './recipes/recipe.service';
 import { AuthInterceptorService } from './auth/auth-intercepter.service'
 import {AlertComponent} from './shared/alert/alert.component'
+
+import { RecipiesModule } from './recipes/recipes.module'
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
     DropdownDirectiveDirective,
-    RecipeStartComponent,
-    RecipeEditComponent,
     AuthComponent,
     LoadingSpinnerComponent,
     AlertComponent
@@ -43,7 +34,8 @@ import {AlertComponent} from './shared/alert/alert.component'
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RecipiesModule
   ],
   providers: [ShoppingListService, RecipeService, { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true }],
   bootstrap: [AppComponent]
