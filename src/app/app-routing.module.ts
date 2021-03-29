442d31f74b6a9a7e8f836b10ed39519e3c22e7d2
Nilesh-agrahyah <nilesh@agrahyah.com>
2021-03-29 16:54:25 +0530

    333. Preloading Lazy-Loaded Code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from "@angular/core"
-import { Routes, RouterModule } from "@angular/router"
+import { Routes, RouterModule, PreloadAllModules } from "@angular/router"
 import {SharedModule} from './shared/shared.module';
 
 const appRoutes: Routes = [
@@ -11,7 +11,7 @@ const appRoutes: Routes = [
 ]
 
 @NgModule({
-    imports: [RouterModule.forRoot(appRoutes), SharedModule],
+    imports: [RouterModule.forRoot(appRoutes, {preloadingStrategy: PreloadAllModules}), SharedModule],
     exports: [RouterModule]
 })
 
